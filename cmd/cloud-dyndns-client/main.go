f1d1918b86ddbeafb0ebdb467d0dd44af1785751
Ian Lewis <ianlewis@google.com>
2017-08-30 18:02:25 +0900

    Moved main.go to cmd/cloud-dyndns-client

--- cmd/cloud-dyndns-client/main.go
+++ cmd/cloud-dyndns-client/main.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package cmd
+package main
 
 import (
 	"context"
@@ -228,4 +228,8 @@ func Main() int {
 	}
 
 	return 0
+}
+
+func main() {
+	os.Exit(Main())
 }
